d7a83009d0ad93f679073e46c4562a07420df8cb
Your Name <you@example.com>
2024-02-25 23:11:48 +0100

    añadiendo footer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
 const router = Router()
 
-router.get('/', (req, res) => res.render('index'))
+router.get('/', (req, res) => res.render('index', { title: 'Primer sitio con NODE'}))
 
-router.get('/about', (req, res) => res.render('about'))
+router.get('/about', (req, res) => res.render('about', { title: 'Este es el about'}))
 
 router.get('/contact', (req, res) => res.render('contact'))
 
